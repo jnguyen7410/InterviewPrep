59dff8caf98582730687a19c4ebadbcafbfcd50d
jnguyen7410 <jnguyen7410@gmail.com>
2018-08-09 22:23:03 -0700

    Added quicksort

--- workspace/TreesAndShit/Sort.java
+++ workspace/TreesAndShit/Sort.java
@@ -51,16 +51,46 @@ public class Sort {
       index++;
     } 
   }
-  
+
+  public static int partition(int[] array, int low, int high) {
+    int pivot = array[high];
+    int i = (low - 1);
+    int temp; //intialized here to save on one memory slot
+    for (int j = low; j < high; j++) {
+      if (array[j] <= pivot) {
+        i++;
+
+        //swap on j and i
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+
+    temp = array[i+1];
+    array[i+1] = array[high];
+    array[high] = temp;
+    
+    return i + 1;
+  }
+
+  public static void quickSort(int[] array, int low, int high) {
+    if (low < high) {
+      int pivot = partition(array, low, high);
+      quickSort(array, low, pivot - 1);
+      quickSort(array, pivot + 1, high);
+    }
+  }
+
   public static boolean inOrder(int[] array) {
     for (int i = 0; i < array.length - 1; i++) {
-      if (array[i] > array[i+1]) {
+      if (array[i] > array[i + 1]) {
         return false;
       }
     }
     return true;
   }
-  
+
   public static String printArray(int[] array) {
     String result = "";
     for (int i = 0; i < array.length; i++) {
@@ -68,17 +98,24 @@ public class Sort {
     }
     return result;
   }
-
-  public static void quickSort(int[] array) {
-    
-  }
   
   public static void main(String[] args) {
+    System.out.println("######## MERGE SORT ########");
+    System.out.println("");
     int[] array = { 1, 8, 6, 5, 8, 10, 28, 18394, 383, 123, 81, 28, 56, 94};
     System.out.println("Before Sorting - In Order: " + inOrder(array));
     System.out.println("array " + printArray(array));
     mergeSort(array, 0, array.length - 1);
     System.out.println("After Sorting - In Order: " + inOrder(array));
     System.out.println("array " + printArray(array));
+    System.out.println("");
+    System.out.println("######## QUICK SORT ########");
+    System.out.println("");
+    int[] array2 = { 1, 8, 6, 5, 8, 10, 28, 18394, 383, 123, 81, 28, 56, 94 };
+    System.out.println("Before Sorting - In Order: " + inOrder(array2));
+    System.out.println("array " + printArray(array2));
+    quickSort(array, 0, array.length - 1);
+    System.out.println("After Sorting - In Order: " + inOrder(array2));
+    System.out.println("array " + printArray(array2));
   }
 }
